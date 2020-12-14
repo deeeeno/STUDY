a1c9d13aaf1b174f04f150b9b8a2e2c10ca7c73c
deeeeno <ksh0512dog@gmail.com>
2020-12-14 18:17:05 +0900

    프로세서 연결하기 문제 런타임 최적화

--- Algorithms/Problem/Codes/SWEA - 프로세서 연결하기/SWEA1767.cpp
+++ Algorithms/Problem/Codes/SWEA - 프로세서 연결하기/SWEA1767.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int map[12][12] = { 0, };
 int N;
 
-typedef struct core{ int x,y; }CORE;
+typedef struct core { int x, y; }CORE;
 CORE coreList[12] = { 0, };
 int coreAmount = 0;
 
@@ -22,9 +22,9 @@ bool already(int x, int y) {
 	return false;
 }
 //backtracking - jongja skill
-int MakeLine(int x, int y, int dir);
+bool MakeLine(int x, int y, int dir);
 void DelLine(int x, int y, int dir);
-
+int GetLength(int dir, int x, int y);
 //문제풀이 함수
 void Function(int test_case);
 
@@ -54,30 +54,52 @@ int main(int argc, char** argv)
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
 
-
+int GetLength(int dir, int x, int y) {
+	switch (dir) {
+	///dir == 0 북
+	case 0:
+		return x;
+		break;
+	//dir == 1 남
+	case 1:
+		return N - x - 1;
+		break;
+	//dir == 2 서
+	case 2:
+		return y;
+		break;
+	//dir == 3 동
+	case 3:
+		return N - y - 1;
+		break;
+	default:
+		return -1;
+	}
+	
+}
 /* 설치 불가한 위치면 -1 반환, 설치 가능하면 전선의 길이 return*/
-int MakeLine(int x, int y, int dir) {
-	int chkX = x+dx[dir], chkY = y+dy[dir];
-	int ret = 0;
+bool MakeLine(int x, int y, int dir) {
+	int directionX = dx[dir], directionY = dy[dir];
+	int chkX = x + directionX, chkY = y + directionY;
 
 	while (chkX >= 0 && chkX < N && chkY >= 0 && chkY < N) {
-		if (map[chkX][chkY] > 0) return -1;
-		chkX += dx[dir]; chkY += dy[dir];
+		if (map[chkX][chkY] > 0) return false;
+		chkX += directionX; chkY += directionY;
 	}
 
-	x += dx[dir]; y += dy[dir];
+	x += directionX; y += directionY;
 	while (x >= 0 && x < N && y >= 0 && y < N) {
 		map[x][y] = 2;
-		++ret;
-		x += dx[dir]; y += dy[dir];
+		x += directionX; y += directionY;
 	}
-	return ret;
+	return true;
 }
 void DelLine(int x, int y, int dir) {
-	x += dx[dir]; y += dy[dir];
+	int directionX = dx[dir], directionY = dy[dir];
+	x += directionX; y += directionY;
 	while (x >= 0 && x < N && y >= 0 && y < N) {
 		map[x][y] = 0;
-		x += dx[dir]; y += dy[dir];
+		x += directionX; y += directionY;
 	}
 }
 
@@ -87,14 +109,16 @@ void Function(int test_case) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			scanf("%d", &map[i][j]);
-			if (map[i][j] == 1 && !already(i, j)) {
-				coreList[coreAmount].x = i;
-				coreList[coreAmount++].y = j;
+			if (map[i][j] == 1) {
+				if (!already(i, j)) {
+					coreList[coreAmount].x = i;
+					coreList[coreAmount++].y = j;
+				}
 			}
 		}
 	}
 	Solution(0, 0, 0);
-	printf("#%d %d\n", test_case, (min_lineAmount==12345678) ? 0 : min_lineAmount);
+	printf("#%d %d\n", test_case, (min_lineAmount == 12345678) ? 0 : min_lineAmount);
 }
 
 void Solution(int coreIndex, int enableCores, int lineAmount) {
@@ -112,15 +136,19 @@ void Solution(int coreIndex, int enableCores, int lineAmount) {
 	}
 
 	//양방향을 따져~
+	bool isWent = false;
 	int make = -1;
-	CORE pivot = coreList[coreIndex];
+	int pivot_x = coreList[coreIndex].x;
+	int pivot_y = coreList[coreIndex].y;
+	
 	for (int dir = 0; dir < 4; dir++) {
-		if ((make = MakeLine(pivot.x, pivot.y, dir)) != -1) {
-			Solution(coreIndex + 1, enableCores + 1, lineAmount + make);
-			DelLine(pivot.x, pivot.y, dir);
+		if (MakeLine(pivot_x, pivot_y, dir)) {
+			isWent = true;
+			Solution(coreIndex + 1, enableCores + 1, lineAmount + GetLength(dir,pivot_x,pivot_y));
+			DelLine(pivot_x, pivot_y, dir);
 		}
-        else{
-            Solution(coreIndex+1,enableCores,lineAmount);
-        }
 	}
+	//아니 이걸 다 들어가고 있었네
+	if (!isWent)
+		Solution(coreIndex + 1, enableCores, lineAmount);
 }
